32488908a2907ae960c06d0dc11ecb68a9ebe04b
Akshaya Sree <akshayasree.puligilla999@gmail.com>
2026-03-30 10:36:42 +0530

    5

--- week3_c.java
+++ week3_c.java
@@ -1,3 +1,121 @@
+import java.util.*;
+
 public class week3_c {
-    
+
+    static int[][] msg = new int[3][1];
+    static int[][] key = new int[3][3];
+    static int[][] cipher = new int[3][1];
+    static int[][] decrypt = new int[3][1];
+    static int[][] inverse = new int[3][3];
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        getkeymes();
+
+        // Encrypt
+        multiplyMatrix(key, msg, cipher);
+
+        System.out.print("\nEncrypted string is:");
+        for (int i = 0; i < 3; i++)
+            System.out.print((char) (cipher[i][0] + 65));
+
+        // Decrypt
+        inverse();
+        multiplyMatrix(inverse, cipher, decrypt);
+
+        System.out.print("\nDecrypted string is:");
+        for (int i = 0; i < 3; i++)
+            System.out.print((char) (decrypt[i][0] + 65));
+    }
+
+    public static void getkeymes() {
+        System.out.println("Enter 3x3 matrix for key:");
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                key[i][j] = sc.nextInt();
+
+        System.out.print("Enter a 3-letter string:");
+        String msgStr = sc.next().toUpperCase();
+
+        for (int i = 0; i < 3; i++)
+            msg[i][0] = msgStr.charAt(i) - 'A';
+    }
+
+    public static void multiplyMatrix(int[][] A, int[][] B, int[][] res) {
+        for (int i = 0; i < 3; i++) {
+            res[i][0] = 0;
+            for (int k = 0; k < 3; k++)
+                res[i][0] += A[i][k] * B[k][0];
+
+            res[i][0] = (res[i][0] % 26 + 26) % 26;
+        }
+    }
+
+    public static void inverse() {
+        int det = determinant(key);
+        int invDet = modInverse(det, 26);
+
+        if (invDet == -1) {
+            System.out.println("Matrix not invertible!");
+            System.exit(0);
+        }
+
+        int[][] adj = adjoint(key);
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                inverse[i][j] = (adj[i][j] * invDet) % 26;
+    }
+
+    public static int determinant(int[][] m) {
+        int det = m[0][0] * (m[1][1]*m[2][2] - m[1][2]*m[2][1])
+                - m[0][1] * (m[1][0]*m[2][2] - m[1][2]*m[2][0])
+                + m[0][2] * (m[1][0]*m[2][1] - m[1][1]*m[2][0]);
+
+        return (det % 26 + 26) % 26;
+    }
+
+    public static int modInverse(int det, int mod) {
+        det = (det % mod + mod) % mod;
+
+        for (int i = 1; i < mod; i++)
+            if ((det * i) % mod == 1)
+                return i;
+
+        return -1;
+    }
+
+    public static int[][] adjoint(int[][] m) {
+        int[][] adj = new int[3][3];
+
+        adj[0][0] = m[1][1]*m[2][2] - m[1][2]*m[2][1];
+        adj[0][1] = m[0][2]*m[2][1] - m[0][1]*m[2][2];
+        adj[0][2] = m[0][1]*m[1][2] - m[0][2]*m[1][1];
+
+        adj[1][0] = m[1][2]*m[2][0] - m[1][0]*m[2][2];
+        adj[1][1] = m[0][0]*m[2][2] - m[0][2]*m[2][0];
+        adj[1][2] = m[0][2]*m[1][0] - m[0][0]*m[1][2];
+
+        adj[2][0] = m[1][0]*m[2][1] - m[1][1]*m[2][0];
+        adj[2][1] = m[0][1]*m[2][0] - m[0][0]*m[2][1];
+        adj[2][2] = m[0][0]*m[1][1] - m[0][1]*m[1][0];
+
+        // transpose
+        for (int i = 0; i < 3; i++)
+            for (int j = i + 1; j < 3; j++) {
+                int t = adj[i][j];
+                adj[i][j] = adj[j][i];
+                adj[j][i] = t;
+            }
+
+        // mod fix
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                adj[i][j] = (adj[i][j] % 26 + 26) % 26;
+
+        return adj;
+    }
 }
--- week5.java
+++ week5.java
@@ -1,3 +1,96 @@
+
 public class week5 {
-    
+
+    private static final int P[] = new int[18]; // P-array
+    private static final int S[][] = new int[4][256]; // S-boxes
+
+    public week5(byte[] key) {
+        keyExpansion(key);
+    }
+
+    // Key Expansion
+    private void keyExpansion(byte[] key) {
+
+        // Initialize P-array and S-boxes (dummy values for simplicity)
+        for (int i = 0; i < 18; i++)
+            P[i] = i;
+
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 256; j++)
+                S[i][j] = j;
+
+        // XOR P-array with key
+        int j = 0;
+        for (int i = 0; i < 18; i++) {
+            P[i] ^= (key[j] << 24) | (key[(j+1)%key.length] << 16) |
+                    (key[(j+2)%key.length] << 8) | key[(j+3)%key.length];
+            j = (j + 4) % key.length;
+        }
+
+        // Initialize using encryption
+        int[] data = {0, 0};
+
+        for (int i = 0; i < 18; i += 2) {
+            encryptBlock(data);
+            P[i] = data[0];
+            P[i+1] = data[1];
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int k = 0; k < 256; k += 2) {
+                encryptBlock(data);
+                S[i][k] = data[0];
+                S[i][k+1] = data[1];
+            }
+        }
+    }
+
+    // Feistel Function
+    private int feistel(int left, int right) {
+
+        int x = left ^ P[0];
+
+        int s0 = S[0][(x >> 24) & 0xFF];
+        int s1 = S[1][(x >> 16) & 0xFF];
+        int s2 = S[2][(x >> 8) & 0xFF];
+        int s3 = S[3][x & 0xFF];
+
+        return ((s0 + s1) ^ (s2 + s3)) ^ right;
+    }
+
+    // Encrypt block
+    public void encryptBlock(int[] block) {
+
+        int left = block[0];
+        int right = block[1];
+
+        for (int i = 0; i < 16; i++) {
+            left ^= feistel(left, right);
+
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+
+        block[0] = right ^ P[17];
+        block[1] = left ^ P[16];
+    }
+
+    // Decrypt block
+    public void decryptBlock(int[] block) {
+
+        int left = block[0];
+        int right = block[1];
+
+        for (int i = 17; i > 1; i--) {
+            left ^= feistel(left, right);
+
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+
+        block[0] = right ^ P[1];
+        block[1] = left ^ P[0];
+    }
 }
